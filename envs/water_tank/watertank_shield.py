bd408972bce336fe7ebb5619ae233e7c6b013f6e
Floris den Hengst <florisdenhengst@gmail.com>
2019-08-06 13:41:43 +0200

    Fix watertank shield and add consistent names for chatbot dfa

--- envs/water_tank/watertank_shield.py
+++ envs/water_tank/watertank_shield.py
@@ -4,7 +4,35 @@ class Shield:
     self.water_level = 0
     self.switch_state = 0
 
-  def tick(self, water_level, switch_state, action):
+  def tick(self, *args, **kwargs):
+    return self.new_tick(*args, **kwargs)
+
+  def new_tick(self, water_level, switch_state, action):
+    if switch_state in [2,3]:
+        return True
+    elif switch_state in [-2,-3]:
+        return False
+    elif 1<= water_level <=3:
+        return True
+    elif 94 <= water_level <= 99:
+        return False
+    else:
+        return action
+
+  def new2_tick(self, water_level, switch_state, action):
+
+    if 1 <= water_level <= 3 and switch_state == 1: 
+        return True
+    elif 97 <= water_level <= 99 and switch_state <= 1:
+        return False
+    elif switch_state in [2,3]:
+        return True
+    elif switch_state in [-2,-3]:
+        return False
+    else:
+        return action
+
+  def original_tick(self, water_level, switch_state, action):
 
     if 1 <= water_level <= 3 and switch_state == 1: 
         return True
